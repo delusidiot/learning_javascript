2a1c5615e4a76940f8f74011a8995acdc71df9b7
delusidiot <delusidiot@gmail.com>
2023-06-12 15:00:37 +0900

    [react] tic tac toe

--- react_library/react-practice/src/index.js
+++ react_library/react-practice/src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import Game from "./Game";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>
 );
 
